44e30520e841497bf34c4e08374827524a701bf4
falau <falau@LENOVO>
2024-12-22 18:36:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Set3Ex14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vector: ");
+            string input = Console.ReadLine();
+            int[] v = Array.ConvertAll(input.Split(" "),int.Parse);
+
+            Console.WriteLine("vector inainte de sortare: ");
+            Print(v);
+
+            MutareZeroLaFinal(v);
+
+            Console.WriteLine("Vector dupa sortare: ");
+            Print(v);
+
+
+
+        }
+        static void MutareZeroLaFinal(int[] v)
+        {
+            int indexNrNenul = 0;
+
+            for(int i  = 0; i < v.Length; i++)
+            {
+                if (v[i] != 0)
+                {
+                    int temp = v[i];
+                    v[i] = v[indexNrNenul];
+                    v[indexNrNenul] = temp;
+
+                    indexNrNenul++;
+
+                }
+            }
+        }
+
+        static void Print(int[] v)
+        {
+            Console.WriteLine(String.Join(" ",v));
+        }
+    }
+}
